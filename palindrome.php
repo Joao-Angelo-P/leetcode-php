3278aaa6bcacaf954cb2a88330cd34885a6453e7
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-06-01 09:20:51 -0400

    Create palindrome.php
    9. Palindrome Number

---
+++ palindrome.php
@@ -0,0 +1,24 @@
+class Solution
+{
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+     /*
+     9. Palindrome Number
+     */
+    function isPalindrome($x)
+    {
+        if ($x < 0 || (!($x % 10) && $x)) {
+            return false;
+        }
+
+        $r = 0;
+        while ($x > $r) {
+            $r = $r * 10 + ($x % 10);
+            $x = floor($x / 10);
+        }
+
+        return in_array($x, [$r, floor($r / 10)]);
+    }
+}

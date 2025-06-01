be6c098dd763c12873b4c9ad023dcb04c36c48df
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-06-01 10:16:19 -0400

    Create typeoftriangle.php
    3024. Type of Triangle

---
+++ typeoftriangle.php
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param Integer[] $nums
+     * @return String
+     */
+     /*
+     3024. Type of Triangle
+     */
+    function triangleType($nums) {
+        sort($nums);
+        [$a, $b, $c]=$nums;
+        if ($a + $b > $c){
+            $nums = count(array_unique($nums));
+            if($nums === 1){
+                return "equilateral";
+            }else{
+                return $nums === 2 ? "isosceles": "scalene";
+            }
+        }else{
+            return "none";
+        }
+
+    }
+}

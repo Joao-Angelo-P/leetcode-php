188bcf80d617d994a89195e490a721f9a75370c0
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-06-01 18:58:45 -0400

    Create ransomnotes.php
    383. Ransom Note

---
+++ ransomnotes.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param String $ransomNote
+     * @param String $magazine
+     * @return Boolean
+     */
+    // 383. Ransom Note
+    function canConstruct($ransomNote, $magazine) {
+        $m = count_chars($magazine, 1);
+        $r = count_chars($ransomNote, 1);
+        foreach ($r as $char => $frequencia) {
+            if(!isset($m[$char]) || $m[$char] < $frequencia) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
